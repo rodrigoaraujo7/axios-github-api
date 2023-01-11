3d4ce87bae897e076b56384e63718ea949c0aeb1
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2023-01-11 17:41:50 -0300

    basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
+type Repository = {
+  full_name: string,
+  description: string
+}
+
 export const App = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/rodrigoaraujo7/repos')
+      .then(response => response.json())
+      .then(data => {
+        setRepositories(data)
+      })
+  }, [])
+
   return (
-    <h1>Hello World!</h1>
+    <ul>
+      {repositories.map(repo => {
+        return (
+          <li key={repo.full_name}>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
+          </li>
+        )
+      })}
+    </ul>
   )
 }
 
